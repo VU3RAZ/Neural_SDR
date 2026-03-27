cc680d05b5c08bf857f7738d41a6a4bef3c3e86c
VU3RAZ <vu3raz@gmail.com>
2026-03-27 11:38:04 +0530

    Delete neuralsdr directory

--- neuralsdr/audio/__init__.py
+++
@@ -1,3 +0,0 @@
-"""audio/__init__.py"""
-from .output import AudioOutput, NullAudioOutput, AudioRingBuffer, make_audio_output
-__all__ = ["AudioOutput", "NullAudioOutput", "AudioRingBuffer", "make_audio_output"]
--- neuralsdr/neural/heads/__init__.py
+++
@@ -1,6 +0,0 @@
-"""neural/heads/__init__.py"""
-from .analog_head  import AnalogHead
-from .digital_head import DigitalHead
-from .squelch_head import SquelchHead
-
-__all__ = ["AnalogHead", "DigitalHead", "SquelchHead"]
